e04f15f3a99d55b9e10d9b3e7110132771f854f0
Camilo E. Hidalgo Estevez <265722+camilohe@users.noreply.github.com>
2023-10-03 20:12:04 +0000

    hashmaps3 - using entry and or_insert

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -39,12 +39,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        // 3
-        let score = get_updated_score(scores.get(&team_1_name), &team_1_name, team_1_score, team_2_score);
-        scores.insert(team_1_name, score);
 
-        let score = get_updated_score(scores.get(&team_2_name), &team_2_name, team_2_score, team_1_score);
-        scores.insert(team_2_name, score);
+        // 4
+        let score = scores.entry(team_1_name).or_insert(Team {goals_scored:0, goals_conceded: 0});
+        score.goals_scored += team_1_score;
+        score.goals_conceded += team_2_score;
+
+        let score = scores.entry(team_2_name).or_insert(Team {goals_scored:0, goals_conceded: 0});
+        score.goals_scored += team_2_score;
+        score.goals_conceded += team_1_score;
+        
+        // 3
+        // let score = get_updated_score(scores.get(&team_1_name), &team_1_name, team_1_score, team_2_score);
+        // scores.insert(team_1_name, score);
+        // let score = get_updated_score(scores.get(&team_2_name), &team_2_name, team_2_score, team_1_score);
+        // scores.insert(team_2_name, score);
 
         // 2
         // let score = match scores.get(&team_1_name) {
